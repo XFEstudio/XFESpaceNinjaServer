3f372f370e820abba4c717ce78a075fdff471c51
XFEstudio <mail@xfegzs.com>
2026-01-19 09:20:29 +0800

    更新启动服务器脚本

--- StartServer.bat
+++ StartServer.bat
@@ -1,3 +1,20 @@
 @echo off
 
-npm run raw
+echo 更新依赖中...
+	node scripts/raw-precheck.js > NUL
+	if %errorlevel% == 0 (
+		call npm i --omit=dev --omit=optional
+		call npm run raw
+	) else (
+		call npm i --omit=dev
+		call npm run build
+		if %errorlevel% == 0 (
+			call npm run start
+		)
+	)
+	echo XFE服务器似乎崩溃了.
+)
+
+:a
+pause > nul
+goto a
